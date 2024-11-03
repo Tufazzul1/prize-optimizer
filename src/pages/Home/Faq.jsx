8a7c8efb5dae30c693bb65106ea12f7260e66aac
Tufazzul1 <tufajjul2017@gmail.com>
2024-11-03 17:52:09 +0600

    Created Review Section

--- src/pages/Home/Faq.jsx
+++ src/pages/Home/Faq.jsx
@@ -1,4 +1,5 @@
 const Faq = () => {
+
     const faqs = [
         {
             question: "Q1: What is PriceOptimizer?",
@@ -33,7 +34,7 @@ const Faq = () => {
     return (
         <div className="sm:px-5 md:px-8 lg:px-16 relative py-16 bg-[#FEF4EA]">
             <img className="absolute right-0 bottom-0" src="/faqbg1.png" alt="faq-img" />
-            <img className="absolute top-[40%] bottom-[60%]" src="/faqbg2.png" alt="faq-img" />
+            <img className="absolute top-[35%] bottom-[65%]" src="/faqbg2.png" alt="faq-img" />
             <h2 className="text-4xl font-Cormorant text-center mb-5 font-semibold">Frequently Asked Questions</h2>
             <div className="max-w-[830px] mx-auto"> 
                 {faqs.map((faq, index) => (
--- src/pages/Home/HomePage/HomePage.jsx
+++ src/pages/Home/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ import Brands from "../Brands";
 import Faq from "../Faq";
 import HowWork from "../HowWork";
 import Result from "../Result";
+import Review from "../Review";
 import Why from "../Why";
 
 
@@ -17,6 +18,7 @@ const HomePage = () => {
             <About></About>
             <Result></Result>
             <Faq></Faq>
+            <Review></Review>
         </div>
     );
 };
